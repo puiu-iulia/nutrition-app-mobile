5033678a52587a314e90cf0d7938ea47149e070a
Iulia P <iulia.puiu@hotmail.com>
2022-07-12 17:09:08 +0300

    Create screen component

--- src/components/screen/Screen.tsx
+++ src/components/screen/Screen.tsx
@@ -1,18 +1,108 @@
-import React from "react"
-import { View, StyleProp, ViewStyle } from 'react-native'
-import  helpers from '../../theme/helpers'
+import React from 'react'
+import { View, StatusBar } from 'react-native'
+//@ts-ignore
+import styled, {css} from 'styled-components'
+import { NavBar } from '../NavBar/NavBar'
 
 interface IScreen {
     children?: JSX.Element | JSX.Element[]
-    style?: StyleProp<ViewStyle>
+    rightButton?: JSX.Element | JSX.Element[]
+    navBarHidden?: boolean
+    leftButtonAction?: () => void
+    rightButtonAction?: () => void
+    leftButtonTitle?: string
+    rightButtonTitle?: string
+    navBarTitle?: string
+    navBarTitleColor?: string
+    navBarTransparent?: boolean
+    backArrowButton?: boolean
+    whiteBackArrowButton?: boolean
+    backgroundColor?: string
+    navBarBackgroundColor?: string
+    lightStatusBar?: boolean
+    closeButton?: boolean
 }
 
-const Screen = ({children, style} : IScreen) => {
+export const Screen = ({children, navBarHidden = false, leftButtonAction, rightButtonAction, leftButtonTitle, rightButtonTitle, rightButton, navBarTitle, navBarTitleColor, navBarTransparent, backArrowButton, backgroundColor, navBarBackgroundColor, whiteBackArrowButton, lightStatusBar, closeButton} : IScreen) => {
     return (
-        <View style={[style, helpers.fill]}>
+        <StyledScreen backgroundColor={backgroundColor}>
+            {lightStatusBar ? <StatusBar barStyle={'light-content'}/>
+             : <StatusBar barStyle={'dark-content'}/>}
+            {!navBarHidden && 
+                <NavBar 
+                    navBarTitleColor={navBarTitleColor}
+                    whiteBackArrowButton={whiteBackArrowButton}
+                    backgroundColor={navBarBackgroundColor}
+                    leftButtonAction={leftButtonAction}
+                    rightButtonAction={rightButtonAction}
+                    leftButtonTitle={leftButtonTitle}
+                    rightButtonTitle={rightButtonTitle}
+                    navBarTitle={navBarTitle}
+                    navBarTransparent={navBarTransparent}
+                    backArrowButton={backArrowButton} 
+                    closeButton={closeButton}
+                    rightButton={rightButton}
+                />
+            }
             {children}
-        </View>
+        </StyledScreen>
     )
 }
 
-export default Screen
+export const ScreenTop = styled(View)``
+export const ScreenBottom = styled(View)`
+    flex: 1;
+    align-items: center;
+    justify-content: flex-end;
+`
+export const ScreenContent = styled(View)`
+    flex: 1;
+    ${({noPadding} : {noPadding?: boolean}) => {
+        if(noPadding) {
+            return css`
+            padding-top: 0px;
+            padding-bottom: 0px;
+            padding-left: 0px;
+            padding-right: 0px;
+        `
+        }
+        return css`
+            padding-top: 15px;
+            padding-bottom: 15px;
+            padding-left: 15px;
+            padding-right: 15px;
+        `
+    }}
+    align-content: flex-start;
+`
+export const ScreenContentCentered = styled(View)`
+    flex: 1;
+    justify-content: center;
+    align-content: flex-start;
+    ${({noPadding} : {noPadding?: boolean}) => {
+        if(noPadding) {
+            return css`
+            padding-top: 0px;
+            padding-bottom: 0px;
+            padding-left: 0px;
+            padding-right: 0px;
+        `
+        }
+        return css`
+            padding-top: 15px;
+            padding-bottom: 15px;
+            padding-left: 15px;
+            padding-right: 15px;
+        `
+    }}
+`
+
+const StyledScreen = styled(View)`
+    flex: 1;
+    background-color: ${({backgroundColor} : {backgroundColor?: string}) => {
+        if(backgroundColor) {
+            return backgroundColor
+        }
+        return '#ffffff'
+    }};
+`
