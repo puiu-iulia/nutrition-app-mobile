901efca033a24a48fe2aa36e5b44e92dafd75d72
Iulia P <iulia.puiu@hotmail.com>
2022-09-24 16:01:15 +0300

    Add Calendar to Meal Plans Screen

--- src/screens/MealPlanner/MealPlannerScreen.tsx
+++ src/screens/MealPlanner/MealPlannerScreen.tsx
@@ -1,10 +1,23 @@
+import moment from 'moment';
 import React from 'react'
 import { View, Text } from 'react-native'
+//@ts-ignore
+import CalendarPicker from 'react-native-calendar-picker';
+import { colors } from '../../theme/generalColors';
 
 const MealPlannerScreen = () => {
     return (
         <View>
             <Text>MealPlannerScreen</Text>
+            <View>
+                <CalendarPicker 
+                    onDateChange={(date: any) => console.log(date)}
+                    startFromMonday={true}
+                    selectedDayColor={colors.primary}
+                    minDate={moment.now()}
+                    restrictMonthNavigation={true}
+                />
+            </View>
         </View>
     )
 }
